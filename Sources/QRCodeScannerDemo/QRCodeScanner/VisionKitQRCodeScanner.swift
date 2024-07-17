698ccb53eaf15b46dffc33355e253c2812ccecf0
penguinsan <penguinsan.pg@gmail.com>
2024-07-17 17:14:01 +0900

    VisionKitQRCodeScanner を作成
    - UIViewControllerRepresentable に準拠する
    - DataScannerViewController を返す

---
+++ Sources/QRCodeScannerDemo/QRCodeScanner/VisionKitQRCodeScanner.swift
@@ -0,0 +1,19 @@
+//
+//  VisionKitQRCodeScanner.swift
+//
+//  
+//  Created by penguinsan on 2024/07/17
+//  
+//
+
+import SwiftUI
+import VisionKit
+
+struct VisionKitQRCodeScanner: UIViewControllerRepresentable {
+
+    func makeUIViewController(context: Context) -> DataScannerViewController {
+    }
+    
+    func updateUIViewController(_ uiViewController: DataScannerViewController, context: Context) {
+    }
+}
