d82d76ac493743946b7cf5ea07b4ac07f20ad418
penguinsan <penguinsan.pg@gmail.com>
2024-07-17 14:31:30 +0900

    DemoRootView を作成
    - Xcode Project に取り込む際はこの画面を呼び出す想定

---
+++ Sources/QRCodeScannerDemo/DemoRootView.swift
@@ -0,0 +1,20 @@
+//
+//  DemoRootView.swift
+//  
+//  
+//  Created by penguinsan on 2024/07/17
+//  
+//
+
+import SwiftUI
+
+struct DemoRootView: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    DemoRootView()
+}
