7046fe598d3c4bae5e0299926ed110c307b28eac
penguinsan <penguinsan.pg@gmail.com>
2024-07-17 18:38:03 +0900

    AVFoundationQRCodeScanner を作成

---
+++ Sources/QRCodeScannerDemo/QRCodeScanner/AVFoundationQRCodeScanner.swift
@@ -0,0 +1,22 @@
+//
+//  AVFoundationQRCodeScanner.swift
+//
+//  
+//  Created by penguinsan on 2024/07/17
+//  
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct AVFoundationQRCodeScanner: UIViewControllerRepresentable {
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        let viewController = UIViewController()
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    }
+}
