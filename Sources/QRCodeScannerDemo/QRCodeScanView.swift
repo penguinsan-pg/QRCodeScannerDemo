c0714c6ed02ab3d7f98b1a38516750ae8a9b95c5
penguinsan <penguinsan.pg@gmail.com>
2024-07-17 14:36:37 +0900

    QRCodeScanView を作成

---
+++ Sources/QRCodeScannerDemo/QRCodeScanView.swift
@@ -0,0 +1,20 @@
+//
+//  QRCodeScanView.swift
+//  
+//  
+//  Created by penguinsan on 2024/07/17
+//  
+//
+
+import SwiftUI
+
+struct QRCodeScanView: View {
+
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    QRCodeScanView()
+}
