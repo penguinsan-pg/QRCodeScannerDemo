c195bbece0e284fe86511601f13e1fbc1a77e9c0
penguinsan <penguinsan.pg@gmail.com>
2024-07-17 14:47:32 +0900

    QRCodeScanView の UI を実装

--- Sources/QRCodeScannerDemo/QRCodeScanView.swift
+++ Sources/QRCodeScannerDemo/QRCodeScanView.swift
@@ -10,8 +10,24 @@ import SwiftUI
 
 struct QRCodeScanView: View {
 
+    @State private var recognizedPayload = ""
+
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { geometry in
+            VStack(spacing: 24) {
+                // TODO: QRコードスキャナの View を配置する
+                Color.orange
+                    .frame(maxWidth: .infinity)
+                    .frame(height: geometry.size.height * 0.7)
+
+                Text("スキャン中のQRコードの値")
+                    .font(.headline)
+
+                Text(recognizedPayload)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+            }
+        }
     }
 }
 
