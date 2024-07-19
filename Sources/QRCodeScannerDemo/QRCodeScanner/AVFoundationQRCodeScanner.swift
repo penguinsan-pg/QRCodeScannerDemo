9cc172f75e8cfcb574a8cff9e6b3f260f997394f
penguinsan <penguinsan.pg@gmail.com>
2024-07-19 10:14:38 +0900

    AVCaptureSession のプロパティを作成し、session の初期設定を行うメソッドを定義する
    - configureSession() はメインスレッドではなく sessionQueue で呼び出す

--- Sources/QRCodeScannerDemo/QRCodeScanner/AVFoundationQRCodeScanner.swift
+++ Sources/QRCodeScannerDemo/QRCodeScanner/AVFoundationQRCodeScanner.swift
@@ -6,17 +6,31 @@
 //  
 //
 
-import Foundation
+import AVFoundation
 import SwiftUI
 import UIKit
 
 struct AVFoundationQRCodeScanner: UIViewControllerRepresentable {
 
+    private let session = AVCaptureSession()
+    private let sessionQueue = DispatchQueue(label: "sessionQueue")
+
     func makeUIViewController(context: Context) -> UIViewController {
         let viewController = UIViewController()
+
+        sessionQueue.async {
+            self.configureSession()
+        }
+
         return viewController
     }
     
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+    }
+}
+
+extension AVFoundationQRCodeScanner {
+
+    private func configureSession() {
     }
 }
